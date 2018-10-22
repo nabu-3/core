3f32c6fd85c51e68be2d01741aab99e1437b1a16
Rafael Gutiérrez <ragutimar@gmail.com>
2018-10-22 16:24:49 +0200

    Modifu script to filter tables

--- sql/filter_cms_site_tables.sql
+++ sql/filter_cms_site_tables.sql
@@ -4,6 +4,12 @@ select * from nb_site where nb_site_id=1
 select * from nb_site_lang where nb_site_id=1 order by nb_language_id
 ;
 
+select * from nb_site_role where nb_site_id=1 order by nb_role_id
+;
+
+select * from nb_site_role_lang where (nb_site_id, nb_role_id) in (select nb_site_id, nb_role_id from nb_site_role where nb_site_id=1) order by nb_language_id
+;
+
 select * from nb_site_target where nb_site_id=1 order by nb_site_target_id
 ;
 
